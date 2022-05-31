e4fab8796b9a4a4d4eb673e0475a295a5bd1b3de
ertugrullberber <ertugrulberber3461@gmail.com>
2022-05-31 21:28:15 +0300

    Initial Commit

---
+++ HeroMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeroMap
+//
+//  Created by Ertugrul Berber on 31.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeroMap/HeroMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroMapApp.swift
+//  HeroMap
+//
+//  Created by Ertugrul Berber on 31.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct HeroMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
